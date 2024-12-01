9cfd96b3e50ee78299e8ed8bc18a6e855f3001b5
Wajih <siai.Wajih@hotmail.com>
2024-12-01 13:21:00 +0100

    jwtconstant-config-change

--- src/main/java/com/siaw/projectmanagementsystem/config/JwtConstant.java
+++ src/main/java/com/siaw/projectmanagementsystem/config/JwtConstant.java
@@ -3,4 +3,5 @@ package com.siaw.projectmanagementsystem.config;
 public class JwtConstant {
     public static final String SECRET_KEY = "fvfxbfdwbgykjdguhetgerghJqZGbdsvsf";
     public static final String JWT_HEADER = "Authorization";
+    //commit config
 }
